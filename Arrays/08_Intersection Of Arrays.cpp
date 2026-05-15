f86f47f7f68d0246182e7d943fc19da9f9fff0c3
Darshan Hegde <darshanhegde1403@gmail.com>
2026-05-15 23:14:09 +0530

    Day 08

--- Arrays/08_Intersection Of Arrays.cpp
+++ Arrays/08_Intersection Of Arrays.cpp
@@ -16,7 +16,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> intersection(vector<int> &a,vector<int> b){
+vector<int> optimal(vector<int> &a,vector<int> b){
     sort(a.begin(),a.end());
     sort(b.begin(),b.end());
     int i=0,j=0;
@@ -33,13 +33,59 @@ vector<int> intersection(vector<int> &a,vector<int> b){
     return res;
 }
 
+vector<int> better(vector<int> a,vector<int> b){
+    unordered_set<int> s1(a.begin(),a.end());
+    unordered_set<int> res_set;
+    
+    for(auto item:b){
+        if(s1.count(item)){
+           res_set.insert(item);
+        }
+    }
+   vector<int> res(res_set.begin(),res_set.end());
+   return res;
+}
+
+vector<int> brute(vector<int> a,vector<int> b){
+    vector<int> res;
+    for(int i=0;i<a.size();i++){
+        bool found=false;
+        for(int k=0;k<res.size();k++){
+            if(res[k]==a[i]) found=true;
+        }
+        if(found) continue;
+        for(int j=0;j<b.size();j++){
+            if(a[i]==b[j]){
+                res.push_back(a[i]);
+                break;
+            }
+        }
+    }
+    return res;
+}
 int main(){
     vector<int> a={1,2,3,6,4,5};
     vector<int> b={2,4,6,7};
-    vector<int> res=intersection(a,b);
+    vector<int> res2=better(a,b);
+    vector<int> res3=brute(a,b);
+    vector<int> res=optimal(a,b);
+    cout<< "Optimal : "<<endl;
     for(auto item:res){
         cout<<item<<" ";
 
     }
+    cout<<endl;
+    cout<< "Better : "<<endl;
+    for(auto item:res2){
+        cout<<item<<" ";
+
+    }
+     cout<<endl;
+    cout<< "Brute : "<<endl;
+    for(auto item:res3){
+        cout<<item<<" ";
+
+    }
+     cout<<endl;
     return 0;
 }
