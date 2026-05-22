0bb43324e34dd4c86e2747dc68035c4244d20044
Darshan Hegde <darshanhegde1403@gmail.com>
2026-05-22 23:59:21 +0530

    Day 15

--- Sum of a Number.cpp
+++ Sum of a Number.cpp
@@ -31,3 +31,16 @@ public:
       return num;
     }
 };
+
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num==0) return 0;
+        if(num%9==0) return 9;
+        else{
+            return num%9;
+            
+        }
+    }
+};
