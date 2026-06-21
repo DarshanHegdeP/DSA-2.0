9248e44ddffba338c862d7f3b0b5853b5d7088a1
Darshan Hegde <darshanhegde1403@gmail.com>
2026-06-21 22:00:04 +0530

    Day 43

--- Strings/43_Reverse String 2.cpp
+++ Strings/43_Reverse String 2.cpp
@@ -1,5 +1,5 @@
 // 541. Reverse String II
-/
+
 // Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
 
 // If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
@@ -11,7 +11,7 @@
 
 // Input: s = "abcd", k = 2
 // Output: "bacd"
- #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
