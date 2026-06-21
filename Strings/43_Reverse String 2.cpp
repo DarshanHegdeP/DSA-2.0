c89809e9daffd33d013d222ac938c7a27d7e6654
Darshan Hegde <darshanhegde1403@gmail.com>
2026-06-21 21:58:10 +0530

    43

---
+++ Strings/43_Reverse String 2.cpp
@@ -0,0 +1,41 @@
+// 541. Reverse String II
+/
+// Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+
+// If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+// Example 1:
+
+// Input: s = "abcdefg", k = 2
+// Output: "bacdfeg"
+// Example 2:
+
+// Input: s = "abcd", k = 2
+// Output: "bacd"
+ #include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string optimal_reverseStr(string s, int k) {
+
+        for(int i=0;i<s.size();i+=2*k){
+            int left=i;
+            int right=min(i+k-1,(int)s.size()-1);
+            while(left<right){
+                swap(s[left],s[right]);
+                left++;
+                right--;
+            }
+        }
+        return s;
+    }
+};
+
+int main()
+{
+    Solution s;
+    string str = "abcdefg";
+    int k = 2;
+    cout << s.optimal_reverseStr(str, k) << endl;
+    return 0;
+}
